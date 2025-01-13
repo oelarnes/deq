b3d81f34ac1bb06bd5322edaf72234b092db5a11
Joel Barnes <oelarnes@gmail.com>
2025-01-12 23:00:23 -0500

    more plotting improvements

--- deq/plot.py
+++ deq/plot.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 
 import numpy as np
 from numpy.typing import NDArray
+
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 import matplotlib.lines as mlines
+import matplotlib.ticker as mtick
+from matplotlib.axes import Axes
 
 from deq.p1_strategy import AnalysisResult
-# default plotly colors
+
 COLOR_LIST = {
     'plotly': ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52'],
     'pyplot': ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'],
@@ -23,7 +25,7 @@ METRIC_LABELS = {
     'gp_wr_bias_adj': 'GP WR Bias Adj.'
 }
 
-FIG_SIZE = (4,3)
+FIG_SIZE = (8,6)
 
 @dataclass
 class StyledSegment:
@@ -38,53 +40,108 @@ class MultiPhaseLineGraph:
     color: str
     segments: list[StyledSegment]
 
-def make_two_phase_line_graph(
-    seed_graph: MultiPhaseLineGraph,
+def two_phase_line_graph(
+    name: str,
+    color: str,
     x: NDArray[np.float64],
     y: NDArray[np.float64],
     q: NDArray[np.bool_],
 ):
-    if max(len(x), len(y), len(q)) <= 1:
-        return seed_graph
-
-    style = 'dot' if not (q[0] and q[1]) else 'solid'
-
-    new_segment = StyledSegment(
-        x=x[0:2],
-        y=y[0:2],
-        style=style
+    assert len(x) == len(y) == len(q), "x, y, q must be the same length"
+    segments = []
+    for i in range(len(x) - 1):
+        style = 'solid' if q[i] and q[i+1] else 'dotted'
+        segments.append(
+            StyledSegment(
+                x = x[i:i+2],
+                y = y[i:i+2],
+                style = style
+            )
+        )
+    
+    return MultiPhaseLineGraph(
+        name=name,
+        color=color,
+        segments=segments,
     )
 
-    graph = MultiPhaseLineGraph(
-        name=seed_graph.name,
-        color=seed_graph.color,
-        segments=[*seed_graph.segments, new_segment]
-    )
-
-    return make_two_phase_line_graph(
-        graph,
-        x[1:],
-        y[1:],
-        q[1:],
-    )
-
-def p1strat_line_graphs(
+def p1strat_line_plot(
     analysis: AnalysisResult,
+    mode: str = 'wr_delta',
     metrics: list[str] | None = None,
-    value_template: str = "{metric}_strat_delta",
-    quality_template: str = "{metric}_misrep",
-    quality_threshold: float = 0.20,
-    colors = 'pyplot'
+    quality_threshold: float = 0.15, # less than this value
+    colors: str = 'pyplot',
+    title_extra: str | None = None,
 ):
     if metrics is None:
         metrics = ['deq', 'gih_wr', 'gp_wr', 'pick_equity', 'iwd']
 
+    config = {
+        'wr_delta': {
+            'title':  'Metric Strategy WR Delta by Skill Cohort',
+            'value_template':  "{metric}_strat_delta",
+            'y_label': 'Simulated Match WR Delta',
+            'y_formatter': mtick.PercentFormatter(1.0, decimals=1)
+        },
+        'misrep': {
+            'title': 'Metric Misrepresentation Index by Skill Cohort',
+            'value_template': "{metric}_misrep",
+            'y_label': 'Misrepresentation Index',
+        }
+    }[mode]
+
+    title = config['title'] + '' if title_extra is None else ' - ' + title_extra
+
+    def graph_fn(i: int, metric: str):
+        x = analysis.df['wr_group'].to_numpy()
+        y = analysis.df[config["value_template"].format(metric=metric)].to_numpy()
+        q = analysis.df[f"{metric}_misrep"].to_numpy() < quality_threshold
+        return two_phase_line_graph(METRIC_LABELS[metric], COLOR_LIST[colors][i], x, y, q)
+
+    graphs = [graph_fn(i, metric) for i, metric in enumerate(metrics)]
+
+    ax = plot_two_phase_series_pyplot(
+        title,
+        graphs,
+        'Skill Cohort',
+        config["y_label"],
+    )
+
+    ax.set_xticks(analysis.df['wr_group'].to_numpy())
+    if 'y_formatter' in config:
+        ax.yaxis.set_major_formatter(config['y_formatter'])
+    ax.xaxis.set_major_formatter(mtick.PercentFormatter(100, decimals=0))
+
+    if mode=="misrep":
+        x = analysis.df["wr_group"]
+        ax.plot(x, [quality_threshold] * len(x), linestyle="dotted", color="gray")
+        ax.text(50, 0.135, "(arbitrary threshold)", color="gray")
+    elif mode=="wr_delta":
+        handles = []
+        for i, metric in enumerate(metrics):
+            value = analysis.agg_df[config["value_template"].format(metric=metric)][0]
+            h, = ax.plot(
+                54, 
+                value, 
+                linestyle="none", 
+                marker="o", 
+                color=COLOR_LIST[colors][i], 
+                label=f"{value * 100:.2f}%"
+            )
+            handles.append(h)
+        leg = ax.get_legend()
+        leg.set_loc(9)
+        ax.legend(handles=handles, loc=1)
+        ax.add_artist(leg)
+    return ax
+
+
 def plot_two_phase_series_pyplot(
     title: str,
-    graphs: list[TwoPhaseLineGraph],
+    graphs: list[MultiPhaseLineGraph],
     x_label: str,
     y_label: str,
-):
+) -> Axes:
     _, ax = plt.subplots(figsize=FIG_SIZE)
 
     ax.set_title(title)
@@ -93,98 +150,16 @@ def plot_two_phase_series_pyplot(
 
     handles = []
     for graph in graphs:
-        for seg in graph.solid_segments:
-                ax.plot(seg[0], seg[1], color=graph.color)
-
-        for seg in graph.dotted_segments:
-            ax.plot(seg[0], seg[1], color=graph.color, linestyle=':') 
+        for seg in graph.segments:
+            ax.plot(
+                seg.x, 
+                seg.y, 
+                color=graph.color, 
+                linestyle=seg.style
+            ) 
 
         handles.append(mlines.Line2D([], [], color=graph.color, label=graph.name))
+
     ax.legend(handles=handles)
 
     return ax
-
-def strat_delta_line_plot(
-    analysis: AnalysisResult,
-    metrics: list[str] | None = None,
-    misrep: bool = False,
-    misrep_threshold: float = .10,
-    display: bool = True,
-    save_to_file: str | None = None,
-) -> go.Figure:
-    df = analysis.df
-    fig = go.Figure()
-
-    for i, metric in enumerate(metrics):
-        label=METRIC_LABELS[metric]
-        color=COLOR_LIST['plotly'][i]
-        value_col = f"{metric}_strat_delta" 
-        
-        x_vals = df['wr_group'].to_numpy()
-        y_vals = df[value_col].to_numpy()
-        quality = df[f"{metric}_misrep"].to_numpy() <= misrep_threshold
-        
-        quality_x = x_vals[quality]
-        if quality_x.size > 0:
-            fig.add_trace(go.Scatter(
-                x=quality_x,
-                y=y_vals[quality],
-                mode='lines',
-                line={'dash': 'solid', 'color': color},
-                showlegend=True,
-                name=label
-            ))
-        
-        if quality_x.size > 0 and not quality[0]:
-            opening_dash_mask = x_vals <= quality_x[0]
-            fig.add_trace(go.Scatter(
-                x=x_vals[opening_dash_mask],
-                y=y_vals[opening_dash_mask],
-                mode='lines',
-                line={'dash': 'dot', 'color': color},
-                showlegend=False,
-                name=label
-            ))
-
-        if quality_x.size > 0 and not quality[-1]:
-            closing_dash_mask = x_vals >= quality_x[-1]
-            fig.add_trace(go.Scatter(
-                x=x_vals[closing_dash_mask],
-                y=y_vals[closing_dash_mask],
-                mode='lines',
-                line={'dash': 'dot', 'color': color},
-                showlegend=False,
-                name=label
-            ))
-
-        if not quality_x.size > 0:
-            fig.add_trace(go.Scatter(
-                x=x_vals,
-                y=y_vals,
-                mode='lines',
-                line={'dash': 'dot', 'color': color},
-                showlegend=True,
-                name=label
-            ))
-
-    fig.update_layout(
-        title='Metric Strategy Delta by WR Group',
-        xaxis_title='WR Group',
-        yaxis_title='Strategy Delta',
-        xaxis=dict(
-            tickmode='array',
-            tickvals=[x for x in range(42, 67, 2)],
-            ticktext=[str(x) for x in range(42, 67, 2)]
-        ),
-        width=800,
-        height=500,
-        margin=dict(l=50, r=50, t=50, b=50),
-        hovermode='x unified',
-        template='plotly_white'
-    )
-
-    if save_to_file:
-        fig.write_html(save_to_file)
-    if display:
-        fig.show()
-    return fig
